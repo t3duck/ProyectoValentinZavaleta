ff189f2633e49061125091cfa6607a8042ef4065
t3duck <124794093+t3duck@users.noreply.github.com>
2023-03-02 07:32:50 -0600

    probando la clase student

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private long controlNumber;
+    private String name;
+    private String lastName;
+    private String career;
+    private short semester;
+
+
+    public Long getControlNumber() {
+        return controlNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public short getSemester() {
+        return semester;
+    }
+
+    public void setControlNumber(Long controlNumber) {
+        this.controlNumber = controlNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public void setSemester(short semester) {
+        this.semester = semester;
+    }
 }
--- src/Test_Student.java
+++ src/Test_Student.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Test_Student {
+public class Test_Student {
+    public static void main(String[] args) {
+        System.out.println("Probando clase student");
+    Student valentin69 = new Student();
+        System.out.println("valentin69 = " + valentin69);
+
+        valentin69.setName("Valentin");
+        System.out.println("El nombre es: " + valentin69.getName());
+        valentin69.setControlNumber(22670011L);
+        System.out.println("El numero de ctrl es :"+ valentin69.getControlNumber());
+    }
 }
